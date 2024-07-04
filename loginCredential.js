8d32fc357401d8edd38254d26e2934b1f910180b
PRANJAY KUMAR <pranjayku123@gmail.com>
2024-07-04 16:47:30 +0530

    fetch method

--- loginCredential.js
+++ loginCredential.js
@@ -1,3 +1,28 @@
 
-const mainContainer = document.querySelector('loginMainWarraper');
-console.log(mainContainer);
+async function getData() {
+    const url = "data.json";
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        // console.log(json);
+        const container = document.getElementById('product-container');
+        json.productKey.forEach(product => {
+            if (product.Addition >= "03") {
+                const productDiv = document.createElement('div');
+                productDiv.classList.add('product');
+                productDiv.innerHTML = `
+                    <h2>${product["Product Name"]}</h2>
+                    <p>Addition: ${product.Addition}</p>
+                    <p>Sub Brand of: ${product["sub brand of"]}</p>
+                    <p>Description: ${product.Description}</p>
+                    <p>Price: ${product.Price}</p>
+                `;
+                container.appendChild(productDiv);
+            }
+        });
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+getData();
